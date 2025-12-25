1caa5fafdbfe4780defefe199ff67670174b6511
boonmilton@gmail.com <boonmilton@gmail.com>
2025-12-25 15:14:55 +0000

    Change ArtistList to use FlexibleColumnLayout

--- app/artistmanagement/webapp/controller/ArtistManagement.controller.js
+++ app/artistmanagement/webapp/controller/ArtistManagement.controller.js
@@ -1,10 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/f/LayoutType"
+], (Controller, JSONModel, LayoutType) => {
     "use strict";
 
     return Controller.extend("artistmanagement.controller.ArtistManagement", {
         onInit() {
+            const detailModel = new JSONModel({ name: "Select an artist", id: "" });
+            this.getView().setModel(detailModel, "detail");
+            this._fcl = this.byId("fcl");
+        },
+
+        onSelectArtist(event) {
+            const context = event.getParameter("listItem").getBindingContext();
+            if (!context) {
+                return;
+            }
+            const data = context.getObject();
+            this.getView().getModel("detail").setData({
+                name: data.name,
+                id: data.ID || data.id || ""
+            });
+            this._fcl.setLayout(LayoutType.TwoColumnsMidExpanded);
+        },
+
+        onCloseDetail() {
+            this._fcl.setLayout(LayoutType.OneColumn);
         }
     });
 });
