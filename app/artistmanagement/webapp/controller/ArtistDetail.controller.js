6c9552878ebb7e0f940905af932b64932cd9998f
boonmilton@gmail.com <boonmilton@gmail.com>
2025-12-25 15:18:49 +0000

    Fix back arrow button not closing Detail page

--- app/artistmanagement/webapp/controller/ArtistDetail.controller.js
+++ app/artistmanagement/webapp/controller/ArtistDetail.controller.js
@@ -6,9 +6,12 @@ sap.ui.define([
 
     return Controller.extend("artistmanagement.controller.ArtistDetail", {
         onCloseDetail() {
-            const fcl = this.getView().getParent();
-            if (fcl && fcl.setLayout) {
-                fcl.setLayout(LayoutType.OneColumn);
+            let parent = this.getView().getParent();
+            while (parent && !parent.isA("sap.f.FlexibleColumnLayout")) {
+                parent = parent.getParent();
+            }
+            if (parent && parent.setLayout) {
+                parent.setLayout(LayoutType.OneColumn);
             }
         }
     });
